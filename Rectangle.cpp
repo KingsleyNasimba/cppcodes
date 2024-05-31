1ec9906b7b0ef7e4fc791f01f1ea769d84cfd335
KingsleyNasimba <167176594+KingsleyNasimba@users.noreply.github.com>
2024-05-31 23:58:12 +0200

    rectangle source file

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -1,30 +1,6 @@
-#include "rectangle.h"
+#include "Rectangle.h"
 
-Rectangle::Rectangle() {
-    length = 0.0;
-    width = 0.0;
-}
-
-Rectangle::~Rectangle() {
-    // Destructor 
-}
-
-void Rectangle::setLength(float len) {
-    length = len;
-}
-
-void Rectangle::setWidth(float wid) {
-    width = wid;
-}
-
-float Rectangle::getLength() const {
-    return length;
-}
-
-float Rectangle::getWidth() const {
-    return width;
-}
-
-float Rectangle::calculateArea() const {
-    return length * width;
+int Rectangle::Area()
+{
+    return mWidth * mHeight;
 }
