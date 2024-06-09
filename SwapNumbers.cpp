a9e6dee090afb97b9754521997cd2bfb68df1163
KingsleyNasimba <167176594+KingsleyNasimba@users.noreply.github.com>
2024-06-09 14:09:25 +0200

    program to swap numbers

---
+++ SwapNumbers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void SwapNumber(int& a, int& b){
+int temp = a;
+a = b;
+b = temp;
+
+}
+
+int main(){
+    int varA = 25;
+    int varB = 100;
+
+    cout << "varA before swap: " << varA << endl; //varA is 25
+     cout << "varB before swap: " << varB << endl; //varA is 100
+
+    SwapNumber(varA, varB);
+
+    cout << "varA after swap: " << varA << endl; //varA is 100
+    cout << "varB after swap: " << varB << endl; //varA is 25
+
+     return 0;
+     
+}
